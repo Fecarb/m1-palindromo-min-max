6b86024ea5214187acb2012578179d4292b6af07
Pedro Ivo Ferreira <pedroivo_fecarb@yahoo.com.br>
2025-02-02 16:57:19 -0300

    entregando exercicio de palindrome, maximo e minimo.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    const textoOriginalSemEspacoEMinusculo = str.replaceAll(' ', '').toLowerCase();
+    let textoInvertido = '';
+    for(let i = textoOriginalSemEspacoEMinusculo.length -1; i >= 0 ; i--){
+        textoInvertido = textoInvertido + textoOriginalSemEspacoEMinusculo[i];
+    }
+    return textoOriginalSemEspacoEMinusculo === textoInvertido;
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let min = arr[0];
+    let max = arr[0];
+
+    for(let i = 0; i < arr.length; i++){
+       if(arr[i] < min){
+          min = arr[i];
+       }
+       if(arr[i] > max){
+          max = arr[i];
+       }
+    }
+
+    return [min,max];
 }
